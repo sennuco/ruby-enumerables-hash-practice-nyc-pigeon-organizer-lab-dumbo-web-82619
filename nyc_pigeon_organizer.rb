32121b3e5d56c75c527ed4e29c321cebbccd890a
Theresa Cyrus <cyrustheresa@gmail.com>
2019-08-09 13:02:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data = data.reduce({}) do |memo, (key, values)|
+    values.each do |item|
+      names = item[1]
+      names.each do |name|
+        if memo[name]
+          if memo[name][key]
+          memo[name][key] << item[0].to_s
+          else
+          memo[name][key] = [item[0].to_s]
+          end
+        else
+          memo[name] = {key => [item[0].to_s]}
+        end
+      end
+    end
+    memo
+  end
+  data
 end
